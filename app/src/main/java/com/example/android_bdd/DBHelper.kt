02bfb8001e9b6a1c3002b226c93203c4ec723712
WarKr0Zz <econte0108@gmail.com>
2022-10-31 13:56:09 +0100

    commit originel

---
+++ app/src/main/java/com/example/android_bdd/DBHelper.kt
@@ -0,0 +1,83 @@
+package com.example.android_bdd
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
+    SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
+
+    // below is the method for creating a database by a sqlite query
+    override fun onCreate(db: SQLiteDatabase) {
+        // below is a sqlite query, where column names
+        // along with their data types is given
+        val query = ("CREATE TABLE " + TABLE_NAME + " ("
+                + ID_COL + " INTEGER PRIMARY KEY, " +
+                NAME_COl + " TEXT," +
+                AGE_COL + " TEXT" + ")")
+
+        // we are calling sqlite
+        // method for executing our query
+        db.execSQL(query)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
+        // this method is to check if table already exists
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
+        onCreate(db)
+    }
+
+    // Méthode pour ajouter a la database
+    fun addName(name : String, age : String ){
+
+
+        val values = ContentValues()
+
+
+        values.put(NAME_COl, name)
+        values.put(AGE_COL, age)
+
+
+        val db = this.writableDatabase
+
+
+        db.insert(TABLE_NAME, null, values)
+
+
+        db.close()
+    }
+
+    //Méthode pour afficher la database
+    fun getName(): Cursor? {
+
+
+        val db = this.readableDatabase
+
+
+        return db.rawQuery("SELECT * FROM " + TABLE_NAME, null)
+
+    }
+
+    companion object{
+
+
+
+        private val DATABASE_NAME = "GEEKS_FOR_GEEKS"
+
+
+        private val DATABASE_VERSION = 1
+
+
+        val TABLE_NAME = "gfg_table"
+
+        val ID_COL = "id"
+
+
+        val NAME_COl = "name"
+
+
+        val AGE_COL = "age"
+    }
+}
